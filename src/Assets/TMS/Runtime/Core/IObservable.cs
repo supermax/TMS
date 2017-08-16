977b063df146078f2a905ffa0ac3a8cfd597621d
Maxim (Mac) <maxim80@gmail.com>
2017-08-17 01:03:31 +0300

    reorganised folders and added #def for UNITY3D

---
+++ src/Assets/TMS/Runtime/Core/IObservable.cs
@@ -0,0 +1,21 @@
+#define UNITY3D
+
+#region Usings
+
+using System;
+using System.ComponentModel;
+
+#endregion
+
+namespace TMS.Common.Core
+{
+	/// <summary>
+	///     Interface for Observable
+	/// </summary>
+	public interface IObservable : IDisposable, INotifyPropertyChanged
+#if !NETFX_CORE && !WINDOWS_PHONE && !SILVERLIGHT && !UNITY3D
+, INotifyPropertyChanging
+#endif
+	{
+	}
+}
